b34a5de2f33ee40a95785d56e0900c156268c506
Arik Mimran <arikm66@yahoo.com>
2026-02-12 21:24:44 +0200

    Fix comment in version.js

--- client/src/version.js
+++ client/src/version.js
@@ -1,3 +1,3 @@
 // Auto-generated version file. Do not edit manually.
-// This file is generated at build/start time to expose the version from package.json
-export const VERSION = "0.5.0";
+// This file is updated by the git pre-commit hook to expose the version from package.json
+export const VERSION = "0.6.0";
