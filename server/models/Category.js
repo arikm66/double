71abda2ea5f6d8a0f6e4226a815d418a77f59206
Arik Mimran <arikm66@yahoo.com>
2026-02-14 20:13:22 +0200

    Add categoryEn and imageUrl fields to Category model

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
+  categoryEn: {
+    type: String,
+    required: [true, 'English category name is required'],
+    trim: true
+  },
   categoryHe: {
     type: String,
     required: [true, 'Hebrew category name is required'],
     trim: true
+  },
+  imageUrl: {
+    type: String,
+    trim: true,
+    default: ''
   }
 }, {
   timestamps: true
